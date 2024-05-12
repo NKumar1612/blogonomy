52bde46b486add324e9b84843ec7b5b606554394
NKumar1612 <nkumar07.nk@gmail.com>
2024-05-12 18:23:43 +0530

    some more changes to the copy text when i press the svg

--- frontend/src/component/SocialsShare.jsx
+++ frontend/src/component/SocialsShare.jsx
@@ -27,38 +27,31 @@ export const SocialsShare = ({ url }) => {
 
     console.log("URL prop:", url);
 
-   
-
     return (
-        <div className='flex space-x-1 items-center'>
-                <TwitterShareButton url={url}>
-                    <TwitterIcon size={32} round={true} />
-                </TwitterShareButton>
-
-                <FacebookShareButton url={url}>
-                    <FacebookIcon size={32} round={true} />
-                </FacebookShareButton>
-
-                <RedditShareButton url={url}>
-                    <RedditIcon size={32} round={true} />
-                </RedditShareButton>
-
-                <WhatsappShareButton url={url}>
-                    <WhatsappIcon size={32} round={true} />
-                </WhatsappShareButton>
-
-                <div>
-                    {copied && (
-                        <div>Copied!</div>
-                        )}
-
-                    <button onClick={copyToClipboard}>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
-                        </svg>
-                    </button>
-                </div>
-                
-        </div>
+        <div className='flex space-x-3 items-center py-2 relative'> 
+            <div className='absolute top-[-7px] left-[11.5rem] flex items-center'> 
+                {copied && (
+                <div className='text-deep-teal text-xs'>Copied!</div>
+                )}
+            </div>
+            
+            <TwitterShareButton url={url}>
+                <TwitterIcon size={32} round={true} />
+            </TwitterShareButton>
+            <FacebookShareButton url={url}>
+                <FacebookIcon size={32} round={true} />
+            </FacebookShareButton>
+            <RedditShareButton url={url}>
+                <RedditIcon size={32} round={true} />
+            </RedditShareButton>
+            <WhatsappShareButton url={url}>
+                <WhatsappIcon size={32} round={true} />
+            </WhatsappShareButton>
+            <button onClick={copyToClipboard}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 1 1.242 7.244" />
+                </svg>
+            </button>
+    </div>
     )
 }
