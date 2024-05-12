0eff9a451be04170b3aa8f7ab2a3a2f494fbe44b
NKumar1612 <nkumar07.nk@gmail.com>
2024-05-12 17:49:46 +0530

    added link button and removed email from social share component.

--- frontend/src/component/SocialsShare.jsx
+++ frontend/src/component/SocialsShare.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { TwitterShareButton, 
-    EmailShareButton,
     FacebookShareButton, 
     RedditShareButton, 
     WhatsappShareButton, 
-    EmailIcon, 
     FacebookIcon, 
     TwitterIcon, 
     WhatsappIcon, 
     RedditIcon} from 'react-share';
 
 export const SocialsShare = ({ url }) => {
+
+    const [copied, setCopied] = useState(false);
+    const currentUrl = window.location.href;
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(currentUrl);
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
+    };
 
     if (!url) {
         console.error("URL prop is missing in ShareButtons component.");
@@ -22,23 +30,35 @@ export const SocialsShare = ({ url }) => {
    
 
     return (
-        <div className='space-x-1'>
+        <div className='flex space-x-1 items-center'>
                 <TwitterShareButton url={url}>
                     <TwitterIcon size={32} round={true} />
                 </TwitterShareButton>
 
-                <EmailShareButton url={url}>
-                    <EmailIcon size={32} round={true} />
-                </EmailShareButton>
                 <FacebookShareButton url={url}>
                     <FacebookIcon size={32} round={true} />
                 </FacebookShareButton>
+
                 <RedditShareButton url={url}>
                     <RedditIcon size={32} round={true} />
                 </RedditShareButton>
+
                 <WhatsappShareButton url={url}>
                     <WhatsappIcon size={32} round={true} />
                 </WhatsappShareButton>
+
+                <div>
+                    {copied && (
+                        <div>Copied!</div>
+                        )}
+
+                    <button onClick={copyToClipboard}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
+                        </svg>
+                    </button>
+                </div>
+                
         </div>
     )
 }
