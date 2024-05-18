7be87d0b7dcfac4a7e2511326a407dd87c85e016
NKumar1612 <nkumar07.nk@gmail.com>
2024-05-18 19:05:52 +0530

    updated domain name of site in cors

--- backend/Index.js
+++ backend/Index.js
@@ -23,7 +23,7 @@ app.use('/uploads', express.static(__dirname+'/uploads'))
 
 mongoose.connect('mongodb+srv://nkumar07nk:JKFJAxY20ydJpyoV@blogonomy.qhscmkr.mongodb.net/?retryWrites=true&w=majority&appName=Blogonomy');
 
-const allowedOrigins = ['https://blogonomy.onrender.com', 'https://blogonomy-1.onrender.com'];
+const allowedOrigins = ['https://blogonomy.onrender.com', 'blogonomy.social'];
 
 app.use(cors({
   origin: function(origin, callback) {
