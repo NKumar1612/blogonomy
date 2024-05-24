7d6cee99e0a8e3ba4e5e0ab5141eba871cb880dc
NKumar1612 <nkumar07.nk@gmail.com>
2024-05-24 12:20:15 +0530

    removed comments.

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -27,22 +27,19 @@ export const PostPage = () => {
 
     return (
         <div className="max-w-4xl mx-auto px-4 py-8">
-            {/* Title (Reduced size, adjusted margins) */}
+
             <h1 className="text-5xl md:text-6xl mb-4 font-playfair italic leading-tight text-deep-burgundy">
                 {postInfo.title}
             </h1>
 
-            {/* Social Share */}
             <SocialsShare url={url} />
 
-            {/* Image (Added max-width for responsiveness) */}
             <img
                 src={`https://blogonomy.onrender.com/${postInfo.cover}`}
                 alt={postInfo.title}
                 className="mb-6 rounded-lg shadow-md max-w-full"
             />
 
-            {/* Metadata (Improved font size and spacing) */}
             <div className="flex items-center text-base text-neutral-gray mb-4 space-x-2">
                 <span>
                 {formatISO9075(new Date(formattedTime))}
@@ -58,7 +55,6 @@ export const PostPage = () => {
                 )}
             </div>
 
-            {/* Content (Ensured text color is deep-burgundy) */}
             <div
                 className="prose max-w-none text-base text-deep-burgundy" 
                 dangerouslySetInnerHTML={{ __html: postInfo.content }}
