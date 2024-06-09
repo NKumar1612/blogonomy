0b6e7d6af5f98b4f3d3f1c5cb909b897466839db
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-09 13:26:16 +0530

    index page media querry changes.

--- frontend/src/pages/IndexPage.jsx
+++ frontend/src/pages/IndexPage.jsx
@@ -18,18 +18,17 @@ export const IndexPage = () => {
       <HeroPage />
 
       <section className="bg-light-lavender py-16 md:py-32">
-        <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <h1 className="text-4xl md:text-5xl font-playfair text-deep-burgundy text-center mb-12 shadow-sm">
             Trending Blogs right now:
           </h1>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"> {/* Adjusted grid classes */}
             {posts.length > 0 &&
               posts.map((post) => (
                 <Post key={post._id} {...post} />
               ))}
           </div>
-
         </div>
       </section>
     </div>
