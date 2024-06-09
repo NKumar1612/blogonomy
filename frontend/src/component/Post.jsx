4582400591764f6c15f116ab011016a31e42191d
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-09 18:03:06 +0530

    post card

--- frontend/src/component/Post.jsx
+++ frontend/src/component/Post.jsx
@@ -11,10 +11,10 @@ export const Post = ({ _id, title, summary, cover, createdAt, author }) => {
     <Link 
       key={_id} 
       to={`/post/${_id}`}
-      className="group flex flex-col md:flex-row bg-soft-white hover:bg-[#F2F0F9] border border-light-lavender rounded-lg overflow-hidden shadow-md transition duration-300 w-80 md:w-full md:max-w-2xl h-80"
+      className="group flex flex-col md:flex-row bg-soft-white hover:bg-[#F2F0F9] border border-light-lavender rounded-lg overflow-hidden shadow-md transition duration-300 w-80 md:w-full md:max-w-2xl"
     >
       {/* Image Section */}
-      <div className="relative flex-shrink-0 w-full md:w-1/2 h-40 md:h-full">
+      <div className="relative flex-shrink-0 w-full md:w-1/2 h-40 md:h-auto">
         <img
           src={`https://blogonomy.onrender.com/${cover}`}
           alt={title}
@@ -25,13 +25,13 @@ export const Post = ({ _id, title, summary, cover, createdAt, author }) => {
       {/* Text Content Section */}
       <div className="w-full md:w-1/2 p-4 md:p-6 flex flex-col justify-between">
         <div>
-          <h2 className="font-playfair text-xl md:text-2xl lg:text-3xl tracking-wide text-deep-burgundy mb-2 line-clamp-2">
+          <h2 className="font-playfair text-xl md:text-2xl lg:text-3xl tracking-wide text-deep-burgundy mb-2">
             {title}
           </h2>
           <p className="text-neutral-gray text-sm mb-2">
             {author.username} | {displayedTime}
           </p>
-          <p className="text-sm line-clamp-3 break-words text-neutral-gray">
+          <p className="text-sm break-words text-neutral-gray">
             {summary}
           </p>
         </div>
@@ -42,8 +42,6 @@ export const Post = ({ _id, title, summary, cover, createdAt, author }) => {
         </div>
       </div>
     </Link>
-
-
 
   );
 };
