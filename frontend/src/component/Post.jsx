d46278366ea301b6424f732acf3c2aebecbdc2fe
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-09 17:29:43 +0530

    first iteration of post code.

--- frontend/src/component/Post.jsx
+++ frontend/src/component/Post.jsx
@@ -8,37 +8,13 @@ export const Post = ({ _id, title, summary, cover, createdAt, author }) => {
   const displayedTime = `${hours}:${minutes}`;
 
   return (
-    <Link 
-      key={_id} 
-      to={`/post/${_id}`}
-      className="group flex flex-col md:grid md:grid-cols-12 bg-soft-white hover:bg-[#F2F0F9] border border-light-lavender rounded-lg overflow-hidden shadow-md transition duration-300 w-80 md:w-full"
-    >
-      {/* Image Section */}
-      <div className="relative md:col-span-4 flex-shrink-0 aspect-square"> 
-        <img
-          src={`https://blogonomy.onrender.com/${cover}`}
-          alt={title}
-          className="object-cover object-center w-full h-full rounded-t-lg md:rounded-l-lg group-hover:scale-105 transition-transform duration-300"
-        />
-      </div>
-
-      {/* Text Content Section */}
-      <div className="md:col-span-8 p-4 md:p-6 flex flex-col flex-grow">
-        <div className="h-full"> {/* Added h-full to this div */}
-          <h2 className="font-playfair text-xl md:text-2xl lg:text-3xl tracking-wide text-deep-burgundy mb-2 line-clamp-2">
-            {title}
-          </h2>
-          <p className="text-neutral-gray text-sm mb-2">
-            {author.username} | {displayedTime}
-          </p>
-          <p className="text-sm line-clamp-3 break-words text-neutral-gray">
-            {summary}
-          </p>
-        </div>
-        <div className="mt-4 self-end">
-          <button className="bg-dark-olive-green text-soft-white font-barlow py-2 px-4 rounded-md hover:bg-darker-mint-green transition-colors duration-300 text-sm">
-            Read More
-          </button>
+    <Link key={_id} to={`/post/${_id}`} className="block rounded-lg overflow-hidden bg-pale-pink hover:shadow-md transition duration-300">
+      <div className="flex items-center">
+        <img className="w-40 h-40 object-cover" src={`https://blogonomy.onrender.com/${cover}`} alt={title} />
+        <div className="p-6 flex flex-col justify-start">
+          <h2 className="font-semibold text-3xl font-barlow mb-2">{title}</h2>
+          <p className="text-sm text-gray-600 mb-2">{author.username} | {displayedTime} </p>
+          <p className="text-sm">{summary}</p>
         </div>
       </div>
     </Link>
