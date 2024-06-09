9bd8e194523e90ad74f7ba4c9b5520142a41a017
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-09 19:22:25 +0530

    hero page

--- frontend/src/component/HeroPage.jsx
+++ frontend/src/component/HeroPage.jsx
@@ -3,18 +3,19 @@ import React from 'react'
 export const 
 HeroPage = () => {
   return (
-    <section className="w-screen h-screen md:h-full bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center px-6 sm:px-10">
-      <div className="py-4 md:py-20 px-4 sm:px-5 text-center"> {/* Adjusted padding */}
-        <h1 className="text-3xl sm:text-4xl md:text-7xl lg:text-8xl font-bold font-playfair leading-tight">
+    <section className="w-screen h-screen md:h-full bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center px-4 sm:px-10">
+      <div className="max-w-md mx-auto py-8 md:py-20 text-center">
+        {/* Added max-w-md for content container and reduced padding */}
+        <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold font-playfair leading-tight mb-2">
           WELCOME TO
         </h1>
-        <h1 className="text-3xl sm:text-4xl md:text-7xl lg:text-8xl font-light mb-2 sm:mb-6 leading-tight">
+        <h1 className="text-3xl md:text-5xl lg:text-7xl font-light mb-4 md:mb-8 leading-tight">
           BLOGONOMY
         </h1>
-        <p className="text-sm md:text-xl font-lato text-neutral-gray max-w-xs sm:max-w-md md:max-w-2xl mx-auto mb-4 sm:mb-6">
+        <p className="text-sm md:text-lg font-lato text-neutral-gray mb-4 md:mb-8">
           Your curated collection of elegant and minimalist blogs. Discover inspiring stories and insightful perspectives.
         </p>
-        <button className="bg-dark-olive-green text-soft-white font-barlow py-2 px-6 rounded-md hover:bg-darker-mint-green transition-colors duration-300 w-full sm:w-auto">
+        <button className="bg-dark-olive-green text-soft-white font-barlow py-2 px-6 rounded-md hover:bg-darker-mint-green transition-colors duration-300 w-full md:w-auto">
           Start Exploring
         </button>
       </div>
