6e1e100573385190e87f08d40cc4a400acdc4388
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-09 13:33:39 +0530

    media querries heropage

--- frontend/src/component/HeroPage.jsx
+++ frontend/src/component/HeroPage.jsx
@@ -3,21 +3,18 @@ import React from 'react'
 export const 
 HeroPage = () => {
   return (
-    <section
-      className="w-screen bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center px-4 sm:px-10"
-    >
-      <div className="py-8 md:py-20 px-5 text-center">
-        
-        <h1 className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl font-bold font-playfair leading-tight">
+    <section className="w-screen h-screen md:h-full bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center px-6 sm:px-10">
+      <div className="py-4 md:py-20 px-4 sm:px-5 text-center"> {/* Adjusted padding */}
+        <h1 className="text-3xl sm:text-4xl md:text-7xl lg:text-8xl font-bold font-playfair leading-tight">
           WELCOME TO
         </h1>
-        <h1 className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl font-light mb-4 sm:mb-6 leading-tight">
+        <h1 className="text-3xl sm:text-4xl md:text-7xl lg:text-8xl font-light mb-2 sm:mb-6 leading-tight">
           BLOGONOMY
         </h1>
-        <p className="text-base md:text-xl font-lato text-neutral-gray max-w-xs sm:max-w-md md:max-w-2xl mx-auto mb-4 sm:mb-6">
+        <p className="text-sm md:text-xl font-lato text-neutral-gray max-w-xs sm:max-w-md md:max-w-2xl mx-auto mb-4 sm:mb-6">
           Your curated collection of elegant and minimalist blogs. Discover inspiring stories and insightful perspectives.
         </p>
-        <button className="bg-dark-olive-green text-soft-white font-barlow py-2 px-6 rounded-md hover:bg-darker-mint-green transition-colors duration-300">
+        <button className="bg-dark-olive-green text-soft-white font-barlow py-2 px-6 rounded-md hover:bg-darker-mint-green transition-colors duration-300 w-full sm:w-auto">
           Start Exploring
         </button>
       </div>
