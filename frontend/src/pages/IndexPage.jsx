23aaa5a9c031941173cf49a83531246050a5e58f
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-09 19:34:05 +0530

    index page

--- frontend/src/pages/IndexPage.jsx
+++ frontend/src/pages/IndexPage.jsx
@@ -18,12 +18,12 @@ export const IndexPage = () => {
       <HeroPage />
 
       <section className="bg-light-lavender py-16 md:py-32">
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="container mx-auto sm:px-6 lg:px-8"> {/* Removed px-4 */}
           <h1 className="text-4xl md:text-5xl font-playfair text-deep-burgundy text-center mb-12 shadow-sm">
             Trending Blogs right now:
           </h1>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"> {/* Adjusted grid classes */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"> 
             {posts.length > 0 &&
               posts.map((post) => (
                 <Post key={post._id} {...post} />
