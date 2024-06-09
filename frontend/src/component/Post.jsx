5651ce100e692b80119d3e400714cb4d99210eb3
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-09 20:11:07 +0530

    media query post card

--- frontend/src/component/Post.jsx
+++ frontend/src/component/Post.jsx
@@ -8,30 +8,30 @@ export const Post = ({ _id, title, summary, cover, createdAt, author }) => {
   const displayedTime = `${hours}:${minutes}`;
 
   return (
-    <Link
-      key={_id}
+    <Link 
+      key={_id} 
       to={`/post/${_id}`}
-      className="group flex flex-col sm:flex-row bg-soft-white hover:bg-[#F2F0F9] border border-light-lavender rounded-lg overflow-hidden shadow-md transition duration-300 w-full sm:max-w-3xl h-64 sm:h-auto"
+      className="group flex flex-col sm:flex-row bg-soft-white hover:bg-[#F2F0F9] border border-light-lavender rounded-lg overflow-hidden shadow-md transition duration-300 w-full sm:max-w-3xl h-auto" // Adjust max-width and height for responsiveness
     >
       {/* Image Section */}
-      <div className="relative flex-shrink-0 w-full md:w-48 h-64 md:h-full">
+      <div className="relative flex-shrink-0 w-full sm:w-1/3 aspect-square"> {/* Make the image container square */}
         <img
           src={`https://blogonomy.onrender.com/${cover}`}
           alt={title}
-          className="object-cover object-center w-full h-full rounded-t-lg md:rounded-t-none md:rounded-l-lg group-hover:scale-105 transition-transform duration-300"
+          className="object-cover object-center w-full h-full rounded-t-lg sm:rounded-l-lg group-hover:scale-105 transition-transform duration-300"
         />
       </div>
 
       {/* Text Content Section */}
-      <div className="flex-grow p-4 md:p-6 flex flex-col justify-between overflow-hidden h-64">
+      <div className="p-4 md:p-6 flex flex-col justify-between flex-grow">
         <div className="flex-grow">
-          <h2 className="font-playfair text-xl md:text-2xl lg:text-3xl tracking-wide text-deep-burgundy mb-2">
+          <h2 className="font-playfair text-xl sm:text-2xl lg:text-3xl tracking-wide text-deep-burgundy mb-2">
             {title}
           </h2>
           <p className="text-neutral-gray text-sm mb-2">
             {author.username} | {displayedTime}
           </p>
-          <p className="text-sm text-neutral-gray line-clamp-1">
+          <p className="text-sm text-neutral-gray line-clamp-3">
             {summary}
           </p>
         </div>
