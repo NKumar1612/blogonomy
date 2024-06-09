29a766c3b9aea4e4831a0334d5808bf7a2181258
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-09 19:15:11 +0530

    post card

--- frontend/src/component/Post.jsx
+++ frontend/src/component/Post.jsx
@@ -8,34 +8,34 @@ export const Post = ({ _id, title, summary, cover, createdAt, author }) => {
   const displayedTime = `${hours}:${minutes}`;
 
   return (
-    <Link 
-      key={_id} 
+    <Link
+      key={_id}
       to={`/post/${_id}`}
-      className="group flex flex-col md:flex-row bg-soft-white hover:bg-[#F2F0F9] border border-light-lavender rounded-lg overflow-hidden shadow-md transition duration-300 w-full md:max-w-3xl h-64 md:h-auto" // Adjust max-width and height for responsiveness
+      className="group flex flex-col md:flex-row bg-soft-white hover:bg-[#F2F0F9] border border-light-lavender rounded-lg overflow-hidden shadow-md transition duration-300 w-full md:max-w-3xl h-64 md:h-auto"
     >
       {/* Image Section */}
-      <div className="relative flex-shrink-0 w-full md:w-1/3"> {/* Removed fixed height, use relative width on desktop */}
-        <img 
+      <div className="relative flex-shrink-0 w-full md:w-48 h-64 md:h-full">
+        <img
           src={`https://blogonomy.onrender.com/${cover}`}
           alt={title}
-          className="object-cover object-center w-full h-full rounded-t-lg md:rounded-l-lg group-hover:scale-105 transition-transform duration-300" 
+          className="object-cover object-center w-full h-full rounded-t-lg md:rounded-t-none md:rounded-l-lg group-hover:scale-105 transition-transform duration-300"
         />
       </div>
 
       {/* Text Content Section */}
-      <div className="p-4 md:p-6 flex flex-col justify-between flex-grow"> 
-        <div className="flex-grow">  {/* Apply flex-grow to this container */}
-          <h2 className="font-playfair text-xl md:text-2xl lg:text-3xl tracking-wide text-deep-burgundy mb-2 line-clamp-2">
+      <div className="flex-grow p-4 md:p-6 flex flex-col justify-between overflow-hidden h-64">
+        <div className="flex-grow">
+          <h2 className="font-playfair text-xl md:text-2xl lg:text-3xl tracking-wide text-deep-burgundy mb-2">
             {title}
           </h2>
           <p className="text-neutral-gray text-sm mb-2">
             {author.username} | {displayedTime}
           </p>
-          <p className="text-sm text-neutral-gray line-clamp-3"> {/* Apply line-clamp to summary */}
+          <p className="text-sm text-neutral-gray line-clamp-1">
             {summary}
           </p>
         </div>
-        <div className="mt-4 flex justify-end"> 
+        <div className="mt-4 flex justify-end">
           <button className="bg-dark-olive-green text-soft-white font-barlow py-2 px-4 rounded-md hover:bg-darker-mint-green transition-colors duration-300 text-sm">
             Read More
           </button>
