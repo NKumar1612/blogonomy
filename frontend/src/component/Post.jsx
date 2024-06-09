6ceb68555021c68d2eb06aac38d7f985fd66155a
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-09 20:16:09 +0530

    media query, index page

--- frontend/src/component/Post.jsx
+++ frontend/src/component/Post.jsx
@@ -11,21 +11,21 @@ export const Post = ({ _id, title, summary, cover, createdAt, author }) => {
     <Link 
       key={_id} 
       to={`/post/${_id}`}
-      className="group flex flex-col sm:flex-row bg-soft-white hover:bg-[#F2F0F9] border border-light-lavender rounded-lg overflow-hidden shadow-md transition duration-300 w-full sm:max-w-3xl h-auto" // Adjust max-width and height for responsiveness
+      className="group flex flex-col bg-soft-white hover:bg-[#F2F0F9] border border-light-lavender rounded-lg overflow-hidden shadow-md transition duration-300 w-80 md:w-full" // Removed grid classes
     >
       {/* Image Section */}
-      <div className="relative flex-shrink-0 w-full sm:w-1/3 aspect-square"> {/* Make the image container square */}
+      <div className="relative flex-shrink-0 aspect-square"> {/* Maintain aspect ratio for mobile */}
         <img
           src={`https://blogonomy.onrender.com/${cover}`}
           alt={title}
-          className="object-cover object-center w-full h-full rounded-t-lg sm:rounded-l-lg group-hover:scale-105 transition-transform duration-300"
+          className="object-cover object-center w-full h-full rounded-t-lg group-hover:scale-105 transition-transform duration-300"
         />
       </div>
 
       {/* Text Content Section */}
       <div className="p-4 md:p-6 flex flex-col justify-between flex-grow">
         <div className="flex-grow">
-          <h2 className="font-playfair text-xl sm:text-2xl lg:text-3xl tracking-wide text-deep-burgundy mb-2">
+          <h2 className="font-playfair text-xl md:text-2xl lg:text-3xl tracking-wide text-deep-burgundy mb-2 line-clamp-2">
             {title}
           </h2>
           <p className="text-neutral-gray text-sm mb-2">
@@ -35,7 +35,7 @@ export const Post = ({ _id, title, summary, cover, createdAt, author }) => {
             {summary}
           </p>
         </div>
-        <div className="mt-4 flex justify-end">
+        <div className="mt-4 self-end">
           <button className="bg-dark-olive-green text-soft-white font-barlow py-2 px-4 rounded-md hover:bg-darker-mint-green transition-colors duration-300 text-sm">
             Read More
           </button>
