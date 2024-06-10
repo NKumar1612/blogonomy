50a319af57ca151d6fdde57076bc11acfb8f4976
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-10 16:47:58 +0530

    hero page mobile/laptop friendly

--- frontend/src/component/HeroPage.jsx
+++ frontend/src/component/HeroPage.jsx
@@ -4,16 +4,16 @@ export const
 HeroPage = () => {
   return (
     <section 
-      className="w-screen h-screen md:min-h-screen bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center px-4 sm:px-10"
+      className="lg:w-screen lg:h-screen sm:min-h-screen bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center px-4 sm:px-10"
     >
-      <div className="max-w-md mx-auto py-8 md:py-20 text-center">
-        <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold font-playfair leading-tight mb-2">
+      <div className="max-w-md mx-auto lg:py-8 sm:py-20 text-center">
+        <h1 className="sm:text-5xl lg:text-7xl font-bold font-playfair leading-tight mb-2">
           WELCOME TO
         </h1>
-        <h1 className="text-3xl md:text-5xl lg:text-7xl font-light mb-4 md:mb-8 leading-tight">
+        <h1 className="sm:text-5xl lg:text-7xl font-light lg:mb-4 sm:mb-8 leading-tight">
           BLOGONOMY
         </h1>
-        <p className="text-sm md:text-lg font-lato text-neutral-gray mb-4 md:mb-8">
+        <p className="lg:text-sm sm:text-lg font-lato text-neutral-gray lg:mb-4 sm:mb-8">
           Your curated collection of elegant and minimalist blogs. Discover inspiring stories and insightful perspectives.
         </p>
         <button className="bg-dark-olive-green text-soft-white font-barlow py-2 px-6 rounded-md hover:bg-darker-mint-green transition-colors duration-300 w-full sm:w-auto">
