5bcd811e4e1d2d0b14a13e709d067168bd9521f0
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-10 20:31:02 +0530

    hero page adjustments

--- frontend/src/component/HeroPage.jsx
+++ frontend/src/component/HeroPage.jsx
@@ -7,17 +7,18 @@ HeroPage = () => {
       className="w-screen h-screen bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center px-4 sm:px-6 md:px-10"
     >
       <div className="flex flex-col max-w-md mx-auto py-4 sm:py-10 md:py-20 text-center">
-        <h1 className="text-4xl sm:text-6xl md:text-7xl font-bold font-playfair mb-2 sm:mb-4">
+        <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold font-playfair mb-4 sm:mb-6">
           WELCOME TO
         </h1>
-        <h1 className="text-4xl sm:text-6xl md:text-7xl font-light mb-2 sm:mb-4 md:mb-6">
+        <h1 className="text-5xl sm:text-6xl md:text-7xl font-light mb-4 sm:mb-6 md:mb-8">
           BLOGONOMY
         </h1>
-        <p className="text-lg sm:text-xl md:text-2xl font-lato text-neutral-gray mb-4 sm:mb-6 md:mb-8">
+        <p className="text-lg sm:text-xl md:text-2xl font-lato text-neutral-gray mb-6 sm:mb-8 md:mb-12">
           Your curated collection of elegant and minimalist blogs. Discover inspiring stories and insightful perspectives.
         </p>
       </div>
     </section>
+
 
 
 
