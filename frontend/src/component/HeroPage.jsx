87417cd5b1f886dd6516fd98322e32473e1a67cd
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-10 20:17:15 +0530

    hero page adjustments.

--- frontend/src/component/HeroPage.jsx
+++ frontend/src/component/HeroPage.jsx
@@ -4,20 +4,21 @@ export const
 HeroPage = () => {
   return (
     <section 
-      className="w-screen h-screen bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center px-4 sm:px-10"
+      className="w-screen h-screen bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center px-4 sm:px-6 md:px-10"
     >
-      <div className="flex flex-col max-w-md mx-auto py-8 lg:py-20 text-center">
-        <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold font-playfair mb-2 sm:mb-4 text-chery-red">
+      <div className="flex flex-col max-w-md mx-auto py-8 sm:py-10 md:py-20 text-center">
+        <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold font-playfair mb-4 sm:mb-6">
           WELCOME TO
         </h1>
-        <h1 className="text-4xl sm:text-5xl lg:text-7xl font-light mb-4 sm:mb-8 lg:mb-12 text-chery-red">
+        <h1 className="text-5xl sm:text-6xl md:text-7xl font-light mb-4 sm:mb-6 md:mb-8">
           BLOGONOMY
         </h1>
-        <p className="text-base sm:text-lg lg:text-xl font-lato text-neutral-gray mb-4 sm:mb-8 lg:mb-12">
+        <p className="text-lg sm:text-xl md:text-2xl font-lato text-neutral-gray mb-6 sm:mb-8 md:mb-12">
           Your curated collection of elegant and minimalist blogs. Discover inspiring stories and insightful perspectives.
         </p>
       </div>
     </section>
+
 
 
   )
