ffdf9949233d3c2599ec4383447dbbba600ee703
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-10 20:11:51 +0530

    hero page fix.

--- frontend/src/component/HeroPage.jsx
+++ frontend/src/component/HeroPage.jsx
@@ -4,13 +4,13 @@ export const
 HeroPage = () => {
   return (
     <section 
-      className="w-screen h-screen bg-gradient-to-r from-pale-pink to-light-lavender text-cherry-red flex flex-col items-center justify-center px-4 sm:px-10"
+      className="w-screen h-screen bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center px-4 sm:px-10"
     >
       <div className="flex flex-col max-w-md mx-auto py-8 lg:py-20 text-center">
-        <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold font-playfair mb-2 sm:mb-4">
+        <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold font-playfair mb-2 sm:mb-4 text-chery-red">
           WELCOME TO
         </h1>
-        <h1 className="text-5xl sm:text-6xl lg:text-7xl font-light mb-4 sm:mb-8 lg:mb-12">
+        <h1 className="text-4xl sm:text-5xl lg:text-7xl font-light mb-4 sm:mb-8 lg:mb-12 text-chery-red">
           BLOGONOMY
         </h1>
         <p className="text-base sm:text-lg lg:text-xl font-lato text-neutral-gray mb-4 sm:mb-8 lg:mb-12">
@@ -18,5 +18,7 @@ HeroPage = () => {
         </p>
       </div>
     </section>
+
+
   )
 }
