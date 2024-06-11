63faca438995baa12824a85ded1e4c7392387a11
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-11 13:46:42 +0530

    test hero background

--- frontend/src/component/HeroPage.jsx
+++ frontend/src/component/HeroPage.jsx
@@ -3,10 +3,16 @@ import React from 'react'
 export const 
 HeroPage = () => {
   return (
-    <section 
-      className="w-screen h-screen bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center px-4 sm:px-6 md:px-10"
-    >
-      <div className="flex flex-col max-w-md mx-auto py-4 sm:py-10 md:py-20 text-center">
+    <section className="relative w-screen h-screen bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center overflow-hidden">
+     
+      <div className="absolute inset-0 flex animate-scroll">
+        <div className="flex-shrink-0 w-full h-full bg-cover bg-[url('https://unsplash.com/photos/a-blurry-photo-of-a-street-light-at-night-t51mvHSr3Rw')]"></div>
+        <div className="flex-shrink-0 w-full h-full bg-cover bg-[url('https://unsplash.com/photos/a-bird-sitting-on-a-branch-of-a-cherry-blossom-tree-G47Mr6j6x9w')]"></div>
+        <div className="flex-shrink-0 w-full h-full bg-cover bg-[url('https://unsplash.com/photos/a-picture-of-a-woman-is-on-display-6ISxg5MviS8')]"></div>
+      </div>
+
+  
+      <div className="relative z-10 flex flex-col max-w-md mx-auto py-4 sm:py-10 md:py-20 text-center bg-gradient-to-r from-pale-pink to-light-lavender bg-opacity-75 rounded-md px-4 sm:px-6 md:px-10">
         <h1 className="text-5xl sm:text-6xl md:text-7xl font-bold font-playfair mb-4 sm:mb-6">
           WELCOME TO
         </h1>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -34,9 +34,19 @@ export default {
       'playfair': ['Playfair Display', 'serif'],
       'barlow': ['Barlow Condensed','sans-serif']
     },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        scroll: 'scroll 30s linear infinite',
+      },
+    },
 
     },
-  },
+    
   plugins: [
     require('@tailwindcss/typography'),
     require('tailwindcss-animate')
