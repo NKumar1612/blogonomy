1c20a9d3fd975d113625f9095372858506fa38f3
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-13 19:52:54 +0530

    posts layout on trending section on index page.

--- frontend/src/pages/IndexPage.jsx
+++ frontend/src/pages/IndexPage.jsx
@@ -17,18 +17,18 @@ export const IndexPage = () => {
     <div>
       <HeroPage />
 
-      <section className="bg-light-lavender lg:w-screen lg:h-screen sm:min-h-screen px-10 py-20">
-          <h1 className="lg:text-4xl sm:text-5xl font-playfair text-deep-burgundy text-center mb-12">
-            Trending Blogs right now:
-          </h1>
+      <section className="bg-light-lavender w-screen min-h-screen px-4 sm:px-10 py-20">
+        <h1 className="text-4xl font-playfair text-deep-burgundy text-center mb-12">
+          Trending Blogs right now:
+        </h1>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8"> 
-            {posts.length > 0 &&
-              posts.map((post) => (
-                <Post key={post._id} {...post} />
-              ))}
-          </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {posts.length > 0 && posts.map((post) => (
+            <Post key={post._id} {...post} />
+          ))}
+        </div>
       </section>
+
     </div>
   );
 }
