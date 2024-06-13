0aecd7c22a3f96a955619cfd5f9f52e5ad868358
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-13 19:58:23 +0530

    testing

--- frontend/src/pages/IndexPage.jsx
+++ frontend/src/pages/IndexPage.jsx
@@ -16,19 +16,17 @@ export const IndexPage = () => {
   return (
     <div>
       <HeroPage />
-
-      <section className="bg-light-lavender w-screen min-h-screen px-4 sm:px-10 py-20">
-        <h1 className="text-4xl font-playfair text-deep-burgundy text-center mb-12">
+      <section className="bg-light-lavender w-screen min-h-screen px-4 sm:px-10 py-10">
+        <h1 className="text-3xl sm:text-5xl font-playfair text-deep-burgundy text-center mb-8">
           Trending Blogs right now:
         </h1>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
           {posts.length > 0 && posts.map((post) => (
             <Post key={post._id} {...post} />
           ))}
         </div>
       </section>
-
     </div>
   );
 }
