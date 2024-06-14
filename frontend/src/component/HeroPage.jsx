bd4238077fd98b0f303056bc1dc426d3c47c6e8e
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-14 20:45:52 +0530

    fade in fade out hero page.

--- frontend/src/component/HeroPage.jsx
+++ frontend/src/component/HeroPage.jsx
@@ -4,10 +4,10 @@ export const
 HeroPage = () => {
   return (
     <section className="relative w-screen h-screen sm:min-h-screen bg-gradient-to-r from-pale-pink to-light-lavender text-chery-red flex flex-col items-center justify-center overflow-hidden">
-      <div className="absolute inset-0 flex">
-        <div className="flex-shrink-0 w-full h-full bg-cover animate-fade-in-out bg-[url('https://images.unsplash.com/photo-1556761175-4b46a572b786')]"></div>
-        <div className="flex-shrink-0 w-full h-full bg-cover animate-fade-in-out animation-delay-5s bg-[url('https://images.unsplash.com/photo-1518837695005-2083093ee35b')]"></div>
-        <div className="flex-shrink-0 w-full h-full bg-cover animate-fade-in-out animation-delay-10s bg-[url('https://images.unsplash.com/photo-1593642532973-d31b6557fa68')]"></div> 
+      <div className="absolute inset-0 flex animate-seamless-scroll">
+        <div className="flex-shrink-0 w-full h-full bg-cover bg-[url('https://images.unsplash.com/photo-1556761175-4b46a572b786')] animate-fade-in-out"></div>
+        <div className="flex-shrink-0 w-full h-full bg-cover bg-[url('https://images.unsplash.com/photo-1518837695005-2083093ee35b')] animate-fade-in-out animation-delay-5s"></div>
+        <div className="flex-shrink-0 w-full h-full bg-cover bg-[url('https://images.unsplash.com/photo-1593642532973-d31b6557fa68')] animate-fade-in-out animation-delay-10s"></div>
       </div>
       {/* Content */}
       <div className="relative z-10 max-w-md mx-auto py-8 sm:py-20 text-center bg-gradient-to-r from-pale-pink to-light-lavender bg-opacity-75 rounded-md px-4 sm:px-6 md:px-10">
