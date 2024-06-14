31c914408e58c27938484490d9f70f5d56877044
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-14 12:02:38 +0530

    test

--- frontend/src/component/SocialsShare.jsx
+++ frontend/src/component/SocialsShare.jsx
@@ -32,7 +32,7 @@ export const SocialsShare = ({ url }) => {
         <div className='flex space-x-3 items-center py-2 relative'> 
             <div className='absolute lg:top-[-7px] sm:top-[-10px] lg:left-[11.5rem] sm:left-[20rem] flex items-center'> 
                 {copied && (
-                <div className='text-deep-teal lg:text-xs sm:text-sm'>Copied!</div>
+                <div className='text-deep-teal lg:text-xs sm:text-base'>Copied!</div>
                 )}
             </div>
             
