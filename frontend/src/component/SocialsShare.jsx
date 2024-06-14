4950b09348971a1989174981adaa716d07b16375
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-14 11:42:50 +0530

    test

--- frontend/src/component/SocialsShare.jsx
+++ frontend/src/component/SocialsShare.jsx
@@ -46,10 +46,10 @@ export const SocialsShare = ({ url }) => {
                 <RedditIcon className='lg:size-8 sm:size-16' round={true} />
             </RedditShareButton>
             <WhatsappShareButton url={url}>
-                <WhatsappIcon className='lg:size-8sm:size-16' round={true}/>
+                <WhatsappIcon className='lg:size-8 sm:size-16' round={true}/>
             </WhatsappShareButton>
             <button onClick={copyToClipboard}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 lg:size-8">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244" />
                 </svg>
             </button>
