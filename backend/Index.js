ced0e502a09a63e02ed646af2103e9480f2c0cfd
NKumar1612 <nkumar07.nk@gmail.com>
2024-06-23 11:10:42 +0530

    index.js revert back.

--- backend/Index.js
+++ backend/Index.js
@@ -1,17 +1,16 @@
-const express      = require('express');
-const cors         = require('cors');
-const mongoose     = require('mongoose');
-const User         = require('./models/User');
-const Post        = require('./models/Post');
-const bcrypt       = require('bcryptjs');
-const jwt          = require('jsonwebtoken');
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const Post = require('./models/Post');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const multer       = require('multer');
-const fs           = require('fs');
-const https        = require('https')
+const multer = require('multer');
+const fs = require('fs');
+const https = require('https');
 
-
-const uploadMiddleware = multer({ dest: 'uploads/' })
+const uploadMiddleware = multer({ dest: 'uploads/' });
 const salt = bcrypt.genSaltSync(10);
 const secret = 'jsgakjsdahs';
 
@@ -19,16 +18,13 @@ const app = express();
 
 // Updated CORS Configuration
 app.use(cors({
-    credentials: true,
-    origin: [
-        
-        'https://blogonomy.social',
-        'https://blogonomy.onrender.com',
-        'https://blogonomy-1.onrender.com'
-
-    ],
+    credentials: true,
+    origin: [
+        'https://blogonomy.social',
+        'https://blogonomy.onrender.com',
+        'https://blogonomy-1.onrender.com'
+    ],
 }));
-
 
 app.use(express.json());
 app.use(cookieParser());
@@ -36,165 +32,149 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 
 mongoose.connect('mongodb+srv://nkumar07nk:JKFJAxY20ydJpyoV@blogonomy.qhscmkr.mongodb.net/?retryWrites=true&w=majority&appName=Blogonomy');
 
-
 app.post('/register', async (req, res) => {
-    const {username,password} = req.body;
+    const { username, password } = req.body;
 
-    try{
-        const userDoc = await User.create({ 
-            username, 
-            password: bcrypt.hashSync(password,salt) 
-        });
-        res.json(userDoc);
-    } catch(e) {
-        res.status(400).json(e);
-    }
-    
+    try {
+        const userDoc = await User.create({
+            username,
+            password: bcrypt.hashSync(password, salt)
+        });
+        res.json(userDoc);
+    } catch (e) {
+        res.status(400).json(e);
+    }
 });
 
 app.post('/login', async (req, res) => {
-
-    const {username,password} = req.body;
-    const userDoc = await User.findOne({username});
-    const passOk = bcrypt.compareSync(password, userDoc.password);
-    if (passOk){
-        //login
-        jwt.sign({ username, id:userDoc._id }, secret, {}, (err, token) => {
-            if (err) throw err;
-            res.cookie('token' , token).json({
-                id: userDoc._id,
-                username,
-
-            });
-        })
-
-    } else{
-        res.status(400).json('wrong credentials.')
-    }
-})
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username });
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+            if (err) throw err;
+            res.cookie('token', token).json({
+                id: userDoc._id,
+                username,
+            });
+        });
+    } else {
+        res.status(400).json('wrong credentials.');
+    }
+});
 
 app.get('/profile', (req, res) => {
-    const token = req.cookies.token; 
-
-    if (!token) {
-        // Handle the case when there's no token (e.g., first load or not logged in)
-        return res.json({ isLoggedIn: false }); // Or another suitable response
-    }
-
-    jwt.verify(token, secret, {}, (err, info) => {
-        if (err) {
-            console.error('JWT verification failed:', err);
-            return res.status(401).json({ error: 'Invalid JWT', isLoggedIn: false });
-        }
-        res.json({ ...info, isLoggedIn: true });
-    });
+    const token = req.cookies.token;
+    if (!token) {
+        return res.json({ isLoggedIn: false });
+    }
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) {
+            console.error('JWT verification failed:', err);
+            return res.status(401).json({ error: 'Invalid JWT', isLoggedIn: false });
+        }
+        res.json({ ...info, isLoggedIn: true });
+    });
 });
 
-app.post('/logout', (req,res) => {
-    res.cookie('token','').json('ok');
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
 });
 
-app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
-    const {originalname,path} = req.file;
-    const parts = originalname.split('.');
-    const ext = parts[parts.length - 1];
-    const newPath = path+'.'+ext;
-    fs.renameSync(path, newPath);
-  
-    const {token} = req.cookies;
-    jwt.verify(token, secret, {}, async (err,info) => {
-      if (err) throw err;
-      const {title,summary,content} = req.body;
-      const postDoc = await Post.create({
-        title,
-        summary,
-        content,
-        cover:newPath,
-        author:info.id,
-      });
-      res.json(postDoc);
-    });
-  
-  });
-  
-  app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
-    let newPath = null;
-    if (req.file) {
-        const { originalname, path } = req.file;
-        const parts = originalname.split('.');
-        const ext = parts[parts.length - 1];
-        newPath = path + '.' + ext;
-        fs.renameSync(path, newPath);
-    }
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
 
-    const { token } = req.cookies;
-    jwt.verify(token, secret, {}, async (err, info) => {
-        if (err) throw err;
-        const { id, title, summary, content } = req.body;
-        
-        // Fetch the post document
-        const postDoc = await Post.findById(id);
-        if (!postDoc) {
-            return res.status(404).json('Post not found');
-        }
-
-        // Check if the user is the author before updating
-        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-        if (!isAuthor) {
-            return res.status(400).json('You are not the author');
-        }
-
-        // Update the post document
-        postDoc.title = title;
-        postDoc.summary = summary;
-        postDoc.content = content;
-        postDoc.cover = newPath ? newPath : postDoc.cover;
-        await postDoc.save();
-
-        res.json(postDoc);
-    });
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { title, summary, content } = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id
+        });
+        res.json(postDoc);
+    });
 });
-  
-  
-app.get('/post', async (req,res) => {
 
-    res.json( await Post.find()
-    .populate('author', ['username'])
-    .sort({createdAt: -1})
-    .limit(20)
-    
-    );
-})
+app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+    let newPath = null;
+    if (req.file) {
+        const { originalname, path } = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+    }
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { id, title, summary, content } = req.body;
+
+        const postDoc = await Post.findById(id);
+        if (!postDoc) {
+            return res.status(404).json('Post not found');
+        }
+
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+        if (!isAuthor) {
+            return res.status(400).json('You are not the author');
+        }
+
+        postDoc.title = title;
+        postDoc.summary = summary;
+        postDoc.content = content;
+        postDoc.cover = newPath ? newPath : postDoc.cover;
+        await postDoc.save();
+
+        res.json(postDoc);
+    });
+});
+
+app.get('/post', async (req, res) => {
+    res.json(await Post.find()
+        .populate('author', ['username'])
+        .sort({ createdAt: -1 })
+        .limit(20)
+    );
+});
 
 app.get('/post/:id', async (req, res) => {
-    const {id} = req.params;
-    const postDoc = await Post.findById(id).populate('author', ['username']);
-    res.json(postDoc);
-})
+    const { id } = req.params;
+    const postDoc = await Post.findById(id).populate('author', ['username']);
+    res.json(postDoc);
+});
 
 exports.handler = async (event, context) => {
-    const url = 'https://blogonomy.onrender.com';
-   
-    return new Promise((resolve, reject) => {
-      const req = https.get(url, (res) => {
-        if (res.statusCode === 200) {
-          resolve({
-            statusCode: 200,
-            body: 'Server pinged successfully',
-          });
-        } else {
-          reject(
-            new Error(`Server ping failed with status code: ${res.statusCode}`)
-          );
-        }
-      });
-   
-      req.on('error', (error) => {
-        reject(error);
-      });
-   
-      req.end();
-    });
-   };
+    const url = 'https://blogonomy.onrender.com';
+
+    return new Promise((resolve, reject) => {
+        const req = https.get(url, (res) => {
+            if (res.statusCode === 200) {
+                resolve({
+                    statusCode: 200,
+                    body: 'Server pinged successfully',
+                });
+            } else {
+                reject(
+                    new Error(`Server ping failed with status code: ${res.statusCode}`)
+                );
+            }
+        });
+
+        req.on('error', (error) => {
+            reject(error);
+        });
+
+        req.end();
+    });
+};
 
 app.listen(10000);
